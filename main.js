7ce165c3c3f458edb5726a00c3d9b832d34b0499
shootTheLuck <shoottheluck@tutanota.com>
2022-02-17 06:16:06 -0800

    Cleanup. Move camera closer

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ const SIZE = 1000;
 const scene = new THREE.Scene();
 
 const renderer = new THREE.WebGLRenderer({antialias: true, logarithmicDepthBuffer: true});
-renderer.shadowMapEnabled = true;
+renderer.shadowMap.enabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 200000);
-camera.position.set(0, SIZE * 0.4, SIZE * 0.8);
+camera.position.set(0, SIZE * 0.3, SIZE * 0.6);
 camera.lookAt(scene.position);
 
 scene.add(new THREE.AmbientLight(0xffffff, 0.2));
@@ -40,72 +40,17 @@ const viewControls = new ViewControls(camera, scene, renderer.domElement);
 
 ////////////////////////////   HeightField   //////////////////////////////
 
-// const noise = new Noise();
 const simplex = new SimplexNoise();
-// noise.seed(8);
 
 function getHeightWithNoise(x, z) {
     // adding 1 here to make value always positive
-    // let value1 = noise.simplex2(x/1400, z/1400) + 1 ;
     const frequency = 5;
     let value1 = simplex.noise(x * frequency / SIZE, z * frequency / SIZE) + 1 ;
     return Math.pow(value1, 3.0) * SIZE/100;
 }
 
-const getHeightWithNoiseNO = (function() {
-
-    const settings = {
-        seed: 8,
-        scale: 3000, //higher is more spread out
-        octaves: 6,
-        persistence: 0.5,
-        lacunarity: 1.75,
-        exponentiation: 1,
-        offsetX: 0,
-        height: 800,
-    }
-
-    // const noise = new Noise();
-    const simplex = new SimplexNoise();
-    // noise.seed(settings.seed);
-
-    return function(x, z, rounding = 0) {
-        let xs = x;
-        let zs = z;
-
-        if (rounding > 0) {
-            // xs = MathUtils.roundToNearest(xs, rounding);
-            // zs = MathUtils.roundToNearest(zs, rounding);
-            xs = Math.round(xs/ rounding) * rounding;
-            zs = Math.round(zs/ rounding) * rounding;
-        }
-        xs /= settings.scale;
-        zs /= settings.scale;
-
-        var frequency = 1;
-        var amplitude = 1;
-        var normalization = 0;
-        var total = 0;
-
-        for (let i = 0; i < settings.octaves; i ++) {
-            var amount = simplex.noise(xs * frequency, zs * frequency) + 0 ;
-            // amount = amount * 0.5 + 0.5;
-            total += amount * amplitude;
-            normalization += amplitude;
-            frequency *= settings.lacunarity;
-            amplitude *= settings.persistence;
-        }
-
-        total /= normalization;
-        total = Math.pow(total, settings.exponentiation);
-        total *= settings.height;
-        return total;
-    }
-})();
-
 function getCoastWithNoise(x, z) {
     // adding 1 here to make value always positive
-    // let value1 = noise.simplex2(x/2000, z/1200) + 1;
     const frequency = 15;
     let value1 = simplex.noise(x * frequency / SIZE, z * frequency / SIZE) + 1 ;
     return Math.pow(value1, 3.0) * SIZE/100;
@@ -119,13 +64,11 @@ const heightFieldMaterial = new THREE.MeshStandardMaterial();
 const heightField = new HeightField(heightFieldGeometry, heightFieldMaterial);
 
 heightField.applyHeightFunction(getHeightWithNoise);
-// heightField.setGeometryDepth(height);
 heightField.clampEdgeHeightSquare({margin: 200, height: -50});
 // heightField.clampEdgeHeightRound({margin: 200, height: -50});
 // heightField.clampEdgeHeightCustom(getCoastWithNoise, {margin: 200, height: -50});
 scene.add(heightField);
 heightField.receiveShadow = true;
-
 
 ////////////////////////////   end HeightField   //////////////////////////////
 
@@ -150,10 +93,10 @@ marker.update = function() {
     const now = Date.now();
     this.position.x = Math.sin(now/2000) * SIZE/4;
     this.position.z = Math.cos(now/2000) * SIZE/4;
+
     // this.position.y = heightField.getHeightAt(this.position.x, this.position.z) + SIZE/201;
     const info = heightField.getInfoAt(this.position.x, this.position.z);
     this.position.y = info.height + SIZE/201;
-    // this.position.y = heightField.getInfoAt(this.position.x, this.position.z) + SIZE/201;
 }
 
 scene.add(marker);
